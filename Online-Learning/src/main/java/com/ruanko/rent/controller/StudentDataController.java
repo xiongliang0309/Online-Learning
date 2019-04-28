f27a71ea2589ea8b4a880e4d3ef042946e4e5afd
xiongliang <xiongliang0309@163.com>
2019-04-28 21:28:12 +0800

    update:文件上传下载大小已解决

--- Online-Learning/src/main/java/com/ruanko/rent/controller/StudentDataController.java
+++ Online-Learning/src/main/java/com/ruanko/rent/controller/StudentDataController.java
@@ -11,8 +11,13 @@ import com.ruanko.rent.service.StudydataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.List;
 
 @Controller
@@ -22,14 +27,60 @@ public class StudentDataController {
 
     //显示学习资料
     @RequestMapping("/student_data")
-    public String showStudentData(Model model){
+    public String showStudentData(Model model) {
         List<Studydata> studydataList = studydataService.getStudydataList();
         model.addAttribute("studydataList", studydataList);
         return "student_data";
     }
 
 
-
-    //删除订单操作
+    //下载学习资料
+    @GetMapping("/studentDownloadFile")
+    @ResponseBody
+    public String teacherDownloadFile(HttpServletRequest request, HttpServletResponse response, String filepath) {
+        String fileName = filepath; // 文件名
+        if (fileName != null) {
+            //设置文件路径
+            File file = new File("C:" + filepath);
+            System.out.println("C:" + filepath);
+            //File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "下载成功";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "下载失败";
+    }
 
 }
